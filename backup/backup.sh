b0a66b5149e78f712dafe5dbf79445abc01e0f5b
Mikle Kolyada <zlogene@gentoo.org>
2020-06-28 22:13:38 +0300

    sync
    Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,7 +3,7 @@
 backup_func() {
 	echo "Runing backup..."
 	for i in \
-	 $(docker images --format="{{.Repository}} {{.ID}}" "unix_ci_*" | cut -d " " -f 1); do docker save ${i} -o ${i}-$(date +"%m-%d-%Y").tar; \
+	 $(docker ps --filter "name=gitea|jenkins|openproject" | tail -n+2 | awk '{  print $1 }'); do docker export -o ${i}-ci-$(date +"%m-%d-%Y").tar ${i}; \
 	done
 }
 
